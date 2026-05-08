18528c2dcd6ca4bf26732ddd38c2036ff7ebd3c1
syeda sakina shahzadi bukhari <sakina0001w@gmail.com>
2026-05-08 21:29:36 +0500

    basics of calculator

---
+++ simple_calculator.py
@@ -0,0 +1,14 @@
+value1 = int(input("Enter your value : "))
+
+value2 = int(input("Enter your value :" ))
+operator = str(input("Enter your operator ""addition","subtraction","Multiplication","Devision"))
+if operator == "addition":
+    print("addition",value1 + value2)
+elif operator == "subtraction":
+    print("subtraction",value1 - value2)
+elif operator == "Multiplication":
+    print("Multiplication",value1 ** value2)
+elif operator == "Devsion":
+    print("Devision",value1 // value2)
+else:
+    print("error")
