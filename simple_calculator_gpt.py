0be91f2613efadb2ff5558e41432de6c06d300ae
syeda sakina shahzadi bukhari <sakina0001w@gmail.com>
2026-05-11 23:25:30 +0500

    update

---
+++ simple_calculator_gpt.py
@@ -0,0 +1,21 @@
+value1 = int(input("Enter your first value: "))
+value2 = int(input("Enter your second value: "))
+
+operator = input(
+    "Enter your operator (addition, subtraction, multiplication, division): "
+)
+
+if operator == "addition":
+    print("Addition =", value1 + value2)
+
+elif operator == "subtraction":
+    print("Subtraction =", value1 - value2)
+
+elif operator == "multiplication":
+    print("Multiplication =", value1 * value2)
+
+elif operator == "division":
+    print("Division =", value1 / value2)
+
+else:
+    print("Error: Invalid operator")
